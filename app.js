a05d7d03c35d4c3067d1c7121c2e2d7b36758134
henley02 <410451447@qq.com>
2018-09-04 20:47:33 +0800

    [+]init

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
 
-console.log(new Date());
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const connectMongo = require('connect-mongo')(session);
+
+app.set('views', path.join(__dirname, 'views'));//
+app.set('view engine', 'ejs');//设置模板引擎
+app.use(express.static(path.join(__dirname, 'public')));//设置静态资源
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser('1231321'));
+
+app.use(session({
+    secret: '1231afasdf',//签名
+    name: 'lisi',// key 的名字 默认是connect.sid
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false},//secure :true 只有https的时候才可以访问，
+    rolling: true,//在每次请求时强行设置 cookie，这将重置 cookie 过期时间（默认：false）
+
+    store: new connectMongo({
+        url: 'mongodb://127.0.0.1:27017/test',
+        touchAfter: 24 * 3600,
+    })
+}))
+
 app.use(function (req, res, next) {
     console.log('--------');
     console.log(new Date());
     next();
 })
 app.get('/', function (req, res) {
-    res.render('index', {
-        title: '1231dfa',
-        list: [1, 2, 3, 4, 5, 6, 7],
-    });
+    if (req.session.userInfo) {
+        res.send("你好" + req.session.userInfo)
+    } else {
+        res.send("未登录")
+    }
 });
 
 app.get('/detail/:id', function (req, res) {
@@ -23,10 +46,38 @@ app.get('/detail/:id', function (req, res) {
     res.send('Hello id!' + id);
 });
 
-app.get('/product', function (req, res) {
-    let params = req.query;
-    console.log(params)
-    res.send('product' + params.a);
+app.get('/loginOut', function (req, res) {
+    // req.session.cookie.maxAge = 0;
+    req.session.destroy(function (err) {
+        cosole.log(err);
+    })
+    res.send("退出成功")
 })
+app.get('/login', function (req, res) {
+    res.cookie("token", 'henley', {maxAge: 60 * 1000, signed: true});
+    console.log(req.signedCookies);
+    req.session.userInfo = "zhagnsan";
+    res.render('login', {
+        title: '登录',
+        cookie1: req.signedCookies.token
+    })
+})
+app.get('/product', function (req, res) {
+    res.send('商品列表')
+})
+app.get('/productAdd', function (req, res) {
+    res.send('添加商品')
+})
+app.get('/productEdit', function (req, res) {
+    res.send('编辑商品列表')
+})
+
+app.post('/doLogin', function (req, res) {
+    console.log(req.body);
+})
+
+app.use(function (req, res) {
+    res.status(404).send('这个是404 路由没有匹配到');
+});
 
 app.listen(3000, '127.0.0.1');
